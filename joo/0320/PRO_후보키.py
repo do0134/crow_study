43289b3475a4a955232b8ee6e8d9963d4ad3eefc
JooJuY <ph2978@naver.com>
2023-03-20 23:01:29 +0900

    0320 programmers 후보키

---
+++ joo/0320/PRO_후보키.py
@@ -0,0 +1,8 @@
+def solution(relation):
+    answer = 0
+    return answer
+
+
+r1 = [["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"],
+      ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]
+print(solution(r1))
