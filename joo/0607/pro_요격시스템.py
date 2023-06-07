7495997104391dd55aea1344a43a9bd2e8c86afd
JooJuY <ph2978@naver.com>
2023-06-07 23:59:33 +0900

    0607 programmers 요격 시스템

--- joo/0607/pro_요격시스템.py
+++ joo/0607/pro_요격시스템.py
@@ -1,4 +1,11 @@
 def solution(targets):
-    answer = 0
-    target.sort(lambda x : -x[1])
+    answer = 1
+    targets.sort()
+    end = targets[0][1]
+    for s, e in targets[1:]:
+        if s >= end:
+            answer += 1
+            end = e
+        elif e <= end:
+            end = e
     return answer
