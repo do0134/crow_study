e4eebb41133cb0b57733b91ee057b61d120b8129
JooJuY <ph2978@naver.com>
2023-06-15 21:52:41 +0900

    0614 programmers 미로탈출명령어

--- joo/0614/pro_미로탈출명령어.py
+++ joo/0614/pro_미로탈출명령어.py
@@ -1,22 +1,34 @@
-import itertools
+import sys
+sys.setrecursionlimit(5000)
+
+letter = ['d', 'l', 'r', 'u']
+dd = [(1, 0), (0, -1), (0, 1), (-1, 0)]
+
+
+def dfs(x, y, n, m, r, c, k, sent, diff):
+    global answer
+    if k == 0 and diff == 0:
+        answer = sent
+        return True
+
+    for i in range(4):
+        nx, ny = x + dd[i][0], y + dd[i][1]
+        if 0 <= nx < n and 0 <= ny < m and diff <= k:
+            if diff % 2 == k % 2:
+                if (dfs(nx, ny, n, m, r, c, k - 1, sent + letter[i],
+                        abs(r - nx) + abs(c - ny))):
+                    return True
+    return False
+
 
 def solution(n, m, x, y, r, c, k):
-    answer = 'impossible'
-    letter = ['d', 'l', 'r', 'u']
-    dd = {'l': (0, -1), 'r': (0, 1), 'u': (-1, 0), 'd': (1, 0)}
-    kinds = list(itertools.product(letter, repeat=k))
-
-    for kind in kinds:
-        flag = 0
-        nx, ny = x, y
-        for kd in kind:
-            nx, ny = nx + dd[kd][0], ny + dd[kd][1]
-            if nx < 1 or nx > n or ny < 1 or ny > m:
-                flag = 1
-                break
-        if flag:
-            continue
-        if nx == r and ny == c:
-            return ''.join(kind)
+    global answer
+    answer = ''
+    dfs(x - 1, y - 1, n, m, r - 1, c - 1, k, '', abs(r - x) + abs(c - y))
+    if answer == '':
+        return 'impossible'
 
     return answer
+
+
+print(solution(3, 4, 2, 3, 3, 1, 5))
